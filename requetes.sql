d883b34bbc8360c390822e6dc6bb8a2c3bebd0bd
Louis | Diox <louishayotte@gmail.com>
2024-11-05 16:52:04 +0100

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -16,10 +16,16 @@ GROUP BY r.id_recipe
 ORDER BY r.prep_time DESC
 
 -- 3
-SELECT recipe_name, prep_time FROM recipe WHERE prep_time < 30 ORDER BY prep_time DESC
+SELECT recipe_name, prep_time 
+FROM recipe 
+WHERE prep_time < 30 
+ORDER BY prep_time DESC
 
 -- 4
-SELECT recipe_name, prep_time, instructions FROM recipe WHERE recipe_name LIKE '%salade%'
+SELECT recipe_name, prep_time, instructions 
+FROM recipe 
+WHERE recipe_name 
+LIKE '%salade%'
 
 -- 5 On Ajoute la recette
 INSERT INTO recipe (recipe_name, prep_time, instructions, id_category) 
@@ -34,7 +40,8 @@ VALUES
 ("crême fraiche", 2.10)
 
 -- 5.3 On lie la recette et les ingrédients
-INSERT INTO recipe_composition VALUES
+INSERT INTO recipe_composition 
+VALUES
 (1, (SELECT id_recipe FROM recipe WHERE recipe_name = 'pâtes à la carbonara'), (SELECT id_ingredient FROM ingredient WHERE ingredient_name = 'oignon rouge'), "entier"),
 (250, (SELECT id_recipe FROM recipe WHERE recipe_name = 'pâtes à la carbonara'), (SELECT id_ingredient FROM ingredient WHERE ingredient_name = 'lardons'), "g"),
 (500, (SELECT id_recipe FROM recipe WHERE recipe_name = 'pâtes à la carbonara'), (SELECT id_ingredient FROM ingredient WHERE ingredient_name = 'pâtes'), "g"),
@@ -45,3 +52,31 @@ INSERT INTO recipe_composition VALUES
 
 
 -- 6
+UPDATE recipe 
+SET recipe_name = "semoule vapeur" 
+WHERE id_recipe = 3
+
+--7
+DELETE 
+FROM recipe 
+WHERE id_recipe = 2
+
+--8
+SELECT ROUND(SUM(ing.price), 2) AS totalprice
+FROM recipe_composition rc
+INNER JOIN ingredient ing 
+ON ing.id_ingredient = rc.id_ingredient
+WHERE id_recipe = 5
+
+-- 9
+SELECT ing.ingredient_name, rc.quantity, ing.price
+FROM recipe_composition rc
+INNER JOIN ingredient ing
+ON ing.id_ingredient = rc.id_ingredient
+WHERE id_recipe = 5
+
+--10
+INSERT INTO ingredient (ingredient_name, price)
+VALUES
+("poivre", 2.5),
+
